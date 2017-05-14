6f2140db7deac643bd2b27911d6a51c3c8b8c212
ClemHlrdt <clement.helardot@gmail.com>
2017-05-14 21:18:46 +0200

    Fixed an error when the author is unknown, and added comment to custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,53 +1,66 @@
 // Random Quote Generator
+
+//URL for the random quote
 var url = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?';
-
+var quoteTwitter;
+//When the document is ready, hide the twitter button
 $(document).ready(function() {
-  //$.getJSON(url, getQuote, 'jsonp');
-  /* Hide twitter share button by default */
-
   $('#twitter-btn').hide();
 });
 
 
-
+//When the user clicks on the button, the event handler makes an Ajax request
 $('#quoteBtn').click(function(event) {
   $.getJSON(url, getQuote, 'jsonp');
 });
 
 
-/*                                                          Function to call a quote                                                                                                        */
+// GetQuote function
 var getQuote = function(data) {
 
-  quote = (data.quoteText);
-  author = (data.quoteAuthor);
-  quoteTwitter = quote + " - " + author;
-  console.log(author + " said " + quote);
-
+  var quote = (data.quoteText); // stores the quote into a variable quote
+  var author = (data.quoteAuthor); //stores the author into the author quote
+  quoteTwitter = quote + " - " + author; //concatenate the quote & the author in order to send it to Twitter
+  //console.log(author + " said " + quote); //Log the check if we get the quote
+  console.log(quote + "\n" +author);
 //  If there's no Author then...
   if (data.quoteAuthor === "") {
-      $("#author").hide('slow');
-      $("#author").show('slow', function() {
-        $(this).text("Unknown");
-      });
-      $("#author").text("Unknown");
+      $("#quote").removeClass("rotateIn"); // removes the class for quote
+      $("#author").removeClass("rotateIn"); // removes the class for the author
+      $('#twitter-btn').removeClass("rubberBand"); // removes the class for the twitter-btn
+
+      $("#quote").show('slow').text("\"" + quote + "\"").addClass('animated rotateIn').css("font-family", 'Montserrat');
+      /* Makes the quote appear slowly, adds the class rotateIn and font Montserrat*/
+      $("#author").show('slow').text("Unknown").addClass('animated rotateIn');
+      /* Adds "Unknown" to the author and adds class rotateIn*/
+      $('#twitter-btn').show().addClass('animated rubberBand');
+      /* makes the twitter button appear and adds a class rubberBand to it */
       var quoteAuthorVar = ("Unknown");
-      console.log("The Author: " + quoteAuthorVar);
+      console.log("The Author: " + quoteAuthorVar); //debug
       //$('#twitter-btn').fadeIn('slow');
+      quoteTwitter = quote + " - " + quoteAuthorVar;
+      /* Function to open new quote and post it on Twitter */
+      $('#twitter-btn').click(function(){
+          var tweetUrl = 'https://twitter.com/share?text=' + quoteTwitter;
+          window.open(tweetUrl);
+      });
 //    If the author is known
   } else {
-    //$("#quote").fadeOut('slow').hide('slow'); //Hide
+    $("#quote").removeClass("rotateIn");  // removes the class for quote
+    $("#author").removeClass("rotateIn"); // removes the class for the author
+    $('#twitter-btn').removeClass("rubberBand");// removes the class for the twitter-btn
 
-    $("#quote").removeClass("rotateIn");
-    $("#author").removeClass("rotateIn");
-    $('#twitter-btn').removeClass("rubberBand");
     $("#quote").show('slow').text("\"" + quote + "\"").addClass('animated rotateIn').css("font-family", 'Montserrat');
+    /* Makes the quote appear slowly, adds the class rotateIn and font Montserrat*/
     $("#author").show('slow').text(author).addClass('animated rotateIn');
+    /* Makes the author appear slowly, adds the class rotateIn*/
     $('#twitter-btn').show().addClass('animated rubberBand');
+    /* Adds "Unknown" to the author and adds class rotateIn*/
+
+    /* Function to open new quote and post it on Twitter */
+    $('#twitter-btn').click(function(){
+        var tweetUrl = 'https://twitter.com/share?text=' + quoteTwitter;
+        window.open(tweetUrl);
+    });
   }
 };
-
-/* Function to open new quote and post it on Twitter */
-function tweetIt () {
-  var tweetUrl = 'https://twitter.com/share?text=' + quoteTwitter;
-  window.open(tweetUrl);
-}
